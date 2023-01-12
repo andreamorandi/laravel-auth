d9fd3b27e438ede1b9c410c2e6f2d6abf1c4e3b2
andreamorandi <morandiandrearocco@gmail.com>
2023-01-12 10:07:29 +0100

    Correzione model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,7 +10,7 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content', 'slug'];
+    protected $fillable = ['title', 'description', 'slug'];
 
     public static function generateSlug($title)
     {
